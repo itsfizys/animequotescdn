9c8591edb5d9dd7c803982c066cee90ddd220f7a
itsfizys <aeroxdevs@gmail.com>
2026-09-19 18:35:36 +0530

    Add PNG output for quote cards

--- api/card.js
+++ api/card.js
@@ -1,7 +1,8 @@
 const { getQuote } = require("../lib/quotes");
 const { quoteCardSvg } = require("../lib/card");
+const sharp = require("sharp");
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const quote = getQuote(req.query || {});
   if (!quote) {
     return res.status(404).json({ error: "No quote found" });
@@ -10,6 +11,14 @@ module.exports = (req, res) => {
   if (req.query.format === "json") {
     return res.status(200).json({ ...quote, format: "svg", svg });
   }
+
+  if (req.query.format === "png") {
+    const png = await sharp(Buffer.from(svg)).png().toBuffer();
+    res.setHeader("Content-Type", "image/png");
+    res.setHeader("Cache-Control", "public, max-age=60, s-maxage=300");
+    return res.status(200).send(png);
+  }
+
   res.setHeader("Content-Type", "image/svg+xml; charset=utf-8");
   res.setHeader("Cache-Control", "public, max-age=60, s-maxage=300");
   return res.status(200).send(svg);
